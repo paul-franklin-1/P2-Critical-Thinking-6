12a163561300c1c83ecd2dcfa0638bb962dda653
Paul Franklin <paul.franklin@csuglobal.edu>
2024-04-28 19:01:56 -0400

    Fantastic development of all classes, closing in on completion

--- src/Student.java
+++ src/Student.java
@@ -10,14 +10,43 @@ public class Student {
         this.studentName = studentName;
         this.studentAddress = studentAddress;}
 
+    public int getRollno() {
+        return rollno;
+    }
+    public static void printAll(Student[] list){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(list[i]);}
+    }
+
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
         Student[] studentList = new Student[10];
         for(int i=0;i<10;i++){
-            Scanner scnr = new Scanner(System.in);
             System.out.println("Enter student Name: ");
             String sName = scnr.nextLine();
             System.out.println("Enter student address: ");
             String sAddress = scnr.nextLine();
             Student student = new Student(sName,sAddress);
-            studentList[i] = student;
-        }}}
+            studentList[i] = student;}
+        boolean continueLoop = true;
+        while(continueLoop){
+            System.out.println("Select option by number: \n" +
+            "1. Sort Students by Roll Number \n" +
+            "2. Sort Students by Name \n" +
+            "3. Print All Student Information \n" +
+            "4. Quit Program");
+            int userChoice = scnr.nextInt();
+            if ((userChoice < 1) || (userChoice > 4)){
+                System.out.println("Please enter integer between 1 and 4");}
+            else if (userChoice == 1){
+                Sortation.sortByRollno(studentList);
+                System.out.println("List has been sorted by Roll Number.");}
+            else if (userChoice == 2){
+                Sortation.sortByName(studentList);
+                System.out.println("List has been sorted by name.");}
+            else if (userChoice == 3){
+                printAll(studentList);}
+            else {
+                continueLoop = false;
+                System.out.println("Goodbye.");}}}}
+
