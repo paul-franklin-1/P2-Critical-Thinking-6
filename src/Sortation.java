2449e9626339c5fc9cdbc6a3f6d8355fd6e48ab5
Paul Franklin <paul.franklin@csuglobal.edu>
2024-04-28 17:54:10 -0400

    Sortation class developed, approaching completion

--- src/Sortation.java
+++ src/Sortation.java
@@ -1,9 +1,18 @@
+import java.util.Comparator;
+
 public class Sortation {
 
-    public void sortByRollno(Student[] listToSort){
-
-    }
-    public void sortByName(Student[] listToSort){
-
-    }
-}
+    public static void sortByRollno(Student[] listToSort, Comparator<Student> rollno){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9 - i; j++) {
+                if (rollno.compare(listToSort[j], listToSort[j + 1]) > 0) {
+                    Student temp = listToSort[j];
+                    listToSort[j] = listToSort[j + 1];
+                    listToSort[j + 1] = temp;}}}}
+    public static void sortByName(Student[] listToSort, Comparator<Student> studentName){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9 - i; j++) {
+               if (studentName.compare(listToSort[j], listToSort[j + 1]) > 0) {
+                   Student temp = listToSort[j];
+                   listToSort[j] = listToSort[j + 1];
+                   listToSort[j + 1] = temp;}}}}}
