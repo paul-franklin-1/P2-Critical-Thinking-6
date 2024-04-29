9ea8789665c399d74fd8d26a76b36f9800ab686a
Paul Franklin <paul.franklin@csuglobal.edu>
2024-04-28 22:55:51 -0400

    final save

--- src/StudentComparatorName.java
+++ src/StudentComparatorName.java
@@ -1,5 +1,9 @@
 import java.util.Comparator;
 
+/**
+ * Empowers the Student class to effectively sort the studentList array
+ * on the basis of student name
+ */
 public class StudentComparatorName implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
--- src/StudentComparatorRollno.java
+++ src/StudentComparatorRollno.java
@@ -1,5 +1,9 @@
 import java.util.Comparator;
 
+/**
+ * Empowers the Student class to effectively sort the studentList array
+ * on the basis of Roll Number
+ */
 public class StudentComparatorRollno implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
